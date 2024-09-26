0f356dbcbe945dee8bdf6f9b23e0f4a9b2c384f9
nniickk <nick0970533868@gmail.com>
2024-09-26 05:05:46 -0700

    add bot_demo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/turtlebot_ws/src
+# Build directory: /home/nick/turtlebot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rplidar_ros")
+subdirs("scan_tools/laser_scan_matcher")
+subdirs("nav")
+subdirs("bot_demo")
---
+++ build/bot_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/turtlebot_ws/src/bot_demo
+# Build directory: /home/nick/turtlebot_ws/build/bot_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
